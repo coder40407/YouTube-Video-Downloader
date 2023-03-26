5ba4e79fa55e182ffaea98ef48651a957d641ec2
coder40407 <128997977+coder40407@users.noreply.github.com>
2023-03-26 23:27:00 +0530

    Add files via upload

---
+++ ytdl/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+import youtube_dl
+from .forms import DownloadForm
+import re
+
+
+def download_video(request):
+    global context
+    form = DownloadForm(request.POST or None)
+    
+    if form.is_valid():
+        video_url = form.cleaned_data.get("url")
+        regex = r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+'
+        #regex = (r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$\n")
+        print(video_url)
+        if not re.match(regex,video_url):
+            print('nhi hoa')
+            return HttpResponse('Enter correct url.')
+
+        # if 'm.' in video_url:
+        #     video_url = video_url.replace(u'm.', u'')
+
+        # elif 'youtu.be' in video_url:
+        #     video_id = video_url.split('/')[-1]
+        #     video_url = 'https://www.youtube.com/watch?v=' + video_id
+
+        # if len(video_url.split("=")[-1]) < 11:
+        #     return HttpResponse('Enter correct url.')
+
+        ydl_opts = {}
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            meta = ydl.extract_info(
+                video_url, download=False)
+        video_audio_streams = []
+        for m in meta['formats']:
+            file_size = m['filesize']
+            if file_size is not None:
+                file_size = f'{round(int(file_size) / 1000000,2)} mb'
+
+            resolution = 'Audio'
+            if m['height'] is not None:
+                resolution = f"{m['height']}x{m['width']}"
+            video_audio_streams.append({
+                'resolution': resolution,
+                'extension': m['ext'],
+                'file_size': file_size,
+                'video_url': m['url']
+            })
+        video_audio_streams = video_audio_streams[::-1]
+        context = {
+            'form': form,
+            'title': meta['title'], 'streams': video_audio_streams,
+            'description': meta['description'], 'likes': meta['like_count'],
+            'dislikes': meta['dislike_count'], 'thumb': meta['thumbnails'][3]['url'],
+            'duration': round(int(meta['duration'])/60, 2), 'views': f'{int(meta["view_count"]):,}'
+        }
+        return render(request, 'home.html', context)
+    return render(request, 'home.html', {'form': form})
